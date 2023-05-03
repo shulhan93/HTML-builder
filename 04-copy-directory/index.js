e78091a892bdd8f2725a6c22df0d0a01e2eb8618
shulhan93 <shulhan.andrei@gmail.com>
2023-05-03 22:36:25 +0300

    fix: added {recursive}

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,11 +2,14 @@ const path = require("path");
 const fs = require("fs/promises");
 
 async function copyDir() {
-  const files = await fs.readdir(path.join(__dirname, "files"), {
+  const srcDir = path.join(__dirname, "files");
+  const outputDir = path.join(__dirname, "files-copy");
+
+  const files = await fs.readdir(srcDir, {
     withFileTypes: true,
   });
 
-  await fs.mkdir(path.join(__dirname, "files-copy"));
+  await fs.mkdir(outputDir, { recursive: true });
 
   for (let file of files) {
     const nameFile = path.basename(file.name);
