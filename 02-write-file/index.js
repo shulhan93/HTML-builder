aba71b3b651328eed880967bcdd5a3bdb59430c7
shulhan93 <shulhan.andrei@gmail.com>
2023-05-03 11:34:33 +0300

    refactor: formatting

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const fs = require("fs");
+const process = require("node:process");
+
+const writeableStream = fs.createWriteStream(
+  path.resolve(__dirname, "txt.txt")
+);
+process.stdout.write("Hello, write some a text... \n");
+
+process.stdin.on("data", (data) => {
+  if (data.toString().trim() == "exit") {
+    process.stdout.write("----- Прощай ------ \n");
+    process.exit();
+  }
+  writeableStream.write(data);
+});
+
+process.on("SIGINT", () => {
+  process.stdout.write("----- Прощай ------ \n");
+  process.exit();
+});
