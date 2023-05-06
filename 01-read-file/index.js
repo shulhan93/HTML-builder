a3c40b38fb1eedf37b9aa91817fed02d641acd93
shulhan93 <shulhan.andrei@gmail.com>
2023-05-06 11:21:00 +0300

    refactor: eslint

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 
-const stream = new fs.createReadStream(path.join(__dirname,  'text.txt'), 'utf-8')
+const stream = new fs.createReadStream(path.join(__dirname,  'text.txt'), 'utf-8');
 
 stream.on('data', (chunk) => {
-  console.log(chunk)
-})
+  console.log(chunk);
+});
 
 stream.on('error', error => console.log('Error', error.message));
